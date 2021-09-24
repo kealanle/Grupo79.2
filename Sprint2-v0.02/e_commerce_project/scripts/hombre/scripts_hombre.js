b7e43e084455c668141f2b39e1dcd846b7b95edb
jimjabid <jabidandresjimenezserrano@gmail.com>
2021-09-23 22:18:47 -0300

    Commit Avance con HTML, CSS, BOOTSTRAP, JAVASCRIPT | PAGINA HOME | PAGINAS CATEGORIAS

---
+++ Sprint2-v0.02/e_commerce_project/scripts/hombre/scripts_hombre.js
@@ -0,0 +1,29 @@
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment()
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("/e_commerce_project/Productos/Hombre/hombre.json");
+    const data = await res.json();
+    pintarCardsHombre(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const pintarCardsHombre = data  => {
+    console.log(data)
+    data.forEach( producto => {
+        templateCard.querySelector('h5').textContent = producto.title
+        templateCard.querySelector('p').textContent = producto.precio
+        templateCard.querySelector('img').setAttribute('src',producto.thumbnailUrl )
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+}
