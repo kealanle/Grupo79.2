19d0c4e118c3faf439f717e95ccd5f4d39a7b3d3
jimjabid <jabidandresjimenezserrano@gmail.com>
2021-10-11 00:04:25 -0300

    Commit Spring Recuperacion

---
+++ Sprint - Recuperacion/src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark"  variant="dark">
+    
+
+      <b-container>
+        <router-link class="navbar-brand" to="/">La Tiendita</router-link>
+      <b-navbar-toggle target="navbar-toggle-collapse">
+      </b-navbar-toggle>
+
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+         <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link class="nav-link" to= "login"
+                ><i class="fas fa-user"></i>Inicie Sesion</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"
+                ><i class="fas fa-shopping-cart"></i>Carrito</a
+              >
+            </li>
+          </ul>
+      </b-navbar-nav>
+      </b-collapse>
+
+  
+    
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ Sprint - Recuperacion/src/views/Accesorios.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="accesorios">
+    <Header/>
+    <NavBar/>
+
+    <div id="seccion" class="container-fluid">
+      <h1 style="text-align: center"></h1>
+      <div class="row" id="items"></div>
+    </div>
+
+
+    <div class="col-12 mb-2 col-md-6 col-lg-4">
+        <div class="card">
+          <img src="" alt="" class="card-img-top" />
+          <div class="card-body">
+            <h5>Titulo</h5>
+            <p>$precio</p>
+            <button class="btn btn-dark">Ver Mas</button>
+          </div>
+        </div>
+    </div>
+
+    <Footer/>
+  
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+      name: 'Accesorios',
+      components: {
+      Header,
+      NavBar,
+      Footer,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
